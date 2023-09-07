9ecb56a9babc66c30df9b8834b1f759cca325d7c
IvaanMiguel <mdominguez_20@alu.uabcs.mx>
2023-09-07 11:27:05 -0700

    Maptile añadido.

---
+++ src/scripts/levelsData.js
@@ -0,0 +1,22 @@
+import { tiles } from './constants.js'
+
+const W = tiles.WALL
+const F = tiles.FLOOR
+
+export const tileMap = [
+  [W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W],
+  [W, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, W],
+  [W, F, W, F, W, F, W, F, W, F, W, F, W, F, W, F, W],
+  [W, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, W],
+  [W, F, W, F, W, F, W, F, W, F, W, F, W, F, W, F, W],
+  [W, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, W],
+  [W, F, W, F, W, F, W, F, W, F, W, F, W, F, W, F, W],
+  [W, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, W],
+  [W, F, W, F, W, F, W, F, W, F, W, F, W, F, W, F, W],
+  [W, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, W],
+  [W, F, W, F, W, F, W, F, W, F, W, F, W, F, W, F, W],
+  [W, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, W],
+  [W, F, W, F, W, F, W, F, W, F, W, F, W, F, W, F, W],
+  [W, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, W],
+  [W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W]
+]
