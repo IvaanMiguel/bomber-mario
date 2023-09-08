4aa9396337af8c229a80b7efdb849add2b9c2b28
IvaanMiguel <mdominguez_20@alu.uabcs.mx>
2023-09-07 21:51:17 -0700

    Mapa de colisones implementado.

--- src/scripts/entities/Player.js
+++ src/scripts/entities/Player.js
@@ -1,6 +1,6 @@
-import { HALF_TILE_SIZE, TILE_SIZE, cornerDirections, direction, movementOrientation, tiles } from '../constants.js';
+import { HALF_TILE_SIZE, TILE_SIZE, cornerDirections, direction, movementOrientation, collisionTile } from '../constants.js';
 import * as controlHandler from '../core/inputHandler.js';
-import { tileMap } from '../levelsData.js';
+import { collisionMap } from '../levelsData.js';
 import Entity from './Entity.js';
 
 const VELOCITY = 120
@@ -43,27 +43,36 @@ class Player {
     }
   }
 
-  getCollisionTile(collisionCoords) { return tileMap[collisionCoords.row][collisionCoords.col] }
+  isBarrierTile(tileValue) { return Object.values(collisionTile.BARRIER).includes(tileValue) }
 
-  coordsShouldBlockMovement(collisionCoords) {
+  getCollisionTile(collisionCoords) { return collisionMap[collisionCoords.row][collisionCoords.col] }
+
+  coordsShouldBlockMovement(collisionCoords, collisionTiles) {
     /* Para verificar que ambas mitades de las coordenadas de colisión estén en el mismo tile. */
     const collisionCoordsMatch = collisionCoords[0].row === collisionCoords[1].row &&
         collisionCoords[0].col === collisionCoords[1].col
-    const collisionTiles = [this.getCollisionTile(collisionCoords[0]), this.getCollisionTile(collisionCoords[1])]
 
-    return (collisionCoordsMatch && collisionTiles[0] === tiles.WALL) ||
-        (collisionTiles[0] === tiles.WALL && collisionTiles[1] === tiles.WALL)
+    return (collisionCoordsMatch && this.isBarrierTile(collisionTiles[0])) ||
+        (this.isBarrierTile() && collisionTiles[1] >= collisionTile.BARRIER.WALL)
   }
 
   checkWallCollision(playerDirection) {
     const collisionCoords = this.getCollisionCoords(playerDirection)
-    if (this.coordsShouldBlockMovement(collisionCoords)) return [direction.DOWN, { x: 0, y: 0 }]
+    const collisionTiles = [
+      this.getCollisionTile(collisionCoords[0]),
+      this.getCollisionTile(collisionCoords[1])
+    ]
+
+    if (this.coordsShouldBlockMovement(collisionCoords, collisionTiles)) {
+      return [direction.DOWN, { x: 0, y: 0 }]
+    }
 
     const alternativeDirections = cornerDirections[playerDirection]
-    if (this.getCollisionTile(collisionCoords[0]) === tiles.WALL) {
+
+    if (this.isBarrierTile(collisionTiles[0])) {
       return [alternativeDirections[0], movementOrientation[alternativeDirections[0]]]
     }
-    if (this.getCollisionTile(collisionCoords[1]) === tiles.WALL) {
+    if (this.isBarrierTile(collisionTiles[1])) {
       return [alternativeDirections[1], movementOrientation[alternativeDirections[1]]]
     }
 
@@ -97,6 +106,7 @@ class Player {
   }
 
   draw(ctx) {
+    ctx.fillStyle = 'black'
     ctx.fillRect(
       this.inst.position.x - HALF_TILE_SIZE,
       this.inst.position.y - HALF_TILE_SIZE,
