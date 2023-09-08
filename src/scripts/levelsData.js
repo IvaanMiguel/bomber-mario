32aee1cb11276df7cee02227f96bee9acdf4d3fd
IvaanMiguel <mdominguez_20@alu.uabcs.mx>
2023-09-07 21:55:12 -0700

    Mapa de colisiones añadido.

--- src/scripts/levelsData.js
+++ src/scripts/levelsData.js
@@ -1,7 +1,11 @@
-import { tiles } from './constants.js'
+import { tile, collisionTile } from './constants.js'
 
-const W = tiles.WALL
-const F = tiles.FLOOR
+const W = tile.WALL
+const F = tile.FLOOR
+
+const CE = collisionTile.EMPTY
+const CW = collisionTile.BARRIER.WALL
+const CB = collisionTile.BARRIER.BLOCK
 
 export const tileMap = [
   [W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W],
@@ -20,3 +24,22 @@ export const tileMap = [
   [W, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, W],
   [W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W]
 ]
+
+export const collisionMap = [
+  [CW, CW, CW, CW, CW, CW, CW, CW, CW, CW, CW, CW, CW, CW, CW, CW, CW],
+  [CW, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CW],
+  [CW, CE, CW, CE, CW, CE, CW, CE, CW, CE, CW, CE, CW, CE, CW, CE, CW],
+  [CW, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CW],
+  [CW, CE, CW, CE, CW, CE, CW, CE, CW, CE, CW, CE, CW, CE, CW, CE, CW],
+  [CW, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CW],
+  [CW, CE, CW, CE, CW, CE, CW, CE, CW, CE, CW, CE, CW, CE, CW, CE, CW],
+  [CW, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CW],
+  [CW, CE, CW, CE, CW, CE, CW, CE, CW, CE, CW, CE, CW, CE, CW, CE, CW],
+  [CW, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CW],
+  [CW, CE, CW, CE, CW, CE, CW, CE, CW, CE, CW, CE, CW, CE, CW, CE, CW],
+  [CW, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CW],
+  [CW, CE, CW, CE, CW, CE, CW, CE, CW, CE, CW, CE, CW, CE, CW, CE, CW],
+  [CW, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CW],
+  [CW, CW, CW, CW, CW, CW, CW, CW, CW, CW, CW, CW, CW, CW, CW, CW, CW]
+]
+
