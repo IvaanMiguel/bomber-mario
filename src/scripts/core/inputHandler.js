61f8d62182e4a9e8e1e9ea908d62190ecd505515
IvaanMiguel <mdominguez_20@alu.uabcs.mx>
2023-09-08 00:05:57 -0700

    Función isAction añadida.

--- src/scripts/core/inputHandler.js
+++ src/scripts/core/inputHandler.js
@@ -34,6 +34,7 @@ export const isUp = key => player_controls[control.UP] === key
 export const isLeft = key => player_controls[control.LEFT] === key
 export const isDown = key => player_controls[control.DOWN] === key
 export const isRight = key => player_controls[control.RIGHT] === key
+export const isAction = key => player_controls[control.ACTION] === key
 
-export const noControlDown = () => heldKeys.size === 0
+// export const noControlDown = () => heldKeys.size === 0
 export const getLastControlDown = () => [...heldKeys][heldKeys.size - 1]
