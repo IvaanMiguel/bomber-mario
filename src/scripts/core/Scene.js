0ff3e83c54bd7e798b6b0cd04e034320078459f2
IvaanMiguel <mdominguez_20@alu.uabcs.mx>
2023-09-08 00:06:59 -0700

    Sistema de bombas añadido a la escena.

--- src/scripts/core/Scene.js
+++ src/scripts/core/Scene.js
@@ -1,6 +1,7 @@
 import { SCREEN_WIDTH, SCREEN_HEIGHT } from '../constants.js'
 import Player from '../entities/Player.js'
 import LevelMap from '../entities/LevelMap.js'
+import BombsSystem from './BombsSystem.js'
 
 class Scene {
   constructor(canvas) {
@@ -8,18 +9,21 @@ class Scene {
     this.ctx = this.canvas.getContext('2d')
     this.ctx.imageSmoothingEnabled = false;
 
-    this.player = new Player({ row: 1, col: 1 })
     this.levelMap = new LevelMap()
+    this.bombsSystem = new BombsSystem()
+    this.player = new Player({ row: 1, col: 1 }, this.bombsSystem.add)
   }
 
   draw() {
     this.ctx.clearRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT)
 
     this.levelMap.draw(this.ctx)
+    this.bombsSystem.draw(this.ctx)
     this.player.draw(this.ctx)
   }
 
   update(time) {
+    this.bombsSystem.update(time)
     this.player.update(time)
   }
 }
