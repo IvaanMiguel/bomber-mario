1b44adc4ecce89dcf8d0fb7df6e4241efcaf703e
IvaanMiguel <mdominguez_20@alu.uabcs.mx>
2023-09-09 09:24:21 -0700

    Código comentado removido.

--- src/scripts/entities/Entity.js
+++ src/scripts/entities/Entity.js
@@ -5,14 +5,6 @@ class Entity {
   constructor(position) {
     this.position = position
   }
-
-  // get positionX() { return this.position.x }
-
-  // set positionX(x) { this.position.x = (x <= 0) ? 0 : x }
-
-  // get positionY() { return this.position.y }
-
-  // set positionY(y) { this.position.y = y }
 }
 
 export default Entity
