82dbe93f6d7c6a79c18e027b7de6a839a9bfebb5
IvaanMiguel <mdominguez_20@alu.uabcs.mx>
2023-09-12 01:07:25 -0700

    Mapas de colisiones eliminados y nuevos tileMaps añadidos.

--- src/scripts/levelsData.js
+++ src/scripts/levelsData.js
@@ -1,45 +1,92 @@
-import { tile, collisionTile } from './constants.js'
+import { tile } from './constants.js'
 
 const W = tile.WALL
 const F = tile.FLOOR
 
-const CE = collisionTile.EMPTY
-const CW = collisionTile.BARRIER.WALL
-const CB = collisionTile.BARRIER.BLOCK
-
-export const tileMap = [
-  [W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W],
-  [W, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, W],
-  [W, F, W, F, W, F, W, F, W, F, W, F, W, F, W, F, W],
-  [W, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, W],
-  [W, F, W, F, W, F, W, F, W, F, W, F, W, F, W, F, W],
-  [W, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, W],
-  [W, F, W, F, W, F, W, F, W, F, W, F, W, F, W, F, W],
-  [W, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, W],
-  [W, F, W, F, W, F, W, F, W, F, W, F, W, F, W, F, W],
-  [W, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, W],
-  [W, F, W, F, W, F, W, F, W, F, W, F, W, F, W, F, W],
-  [W, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, W],
-  [W, F, W, F, W, F, W, F, W, F, W, F, W, F, W, F, W],
-  [W, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, W],
-  [W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W]
+export default [
+  [
+    [W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W],
+    [W, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, W],
+    [W, F, W, F, W, F, W, F, W, F, W, F, W, F, W, F, W],
+    [W, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, W],
+    [W, F, W, F, W, F, W, F, W, F, W, F, W, F, W, F, W],
+    [W, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, W],
+    [W, F, W, F, W, F, W, F, W, F, W, F, W, F, W, F, W],
+    [W, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, W],
+    [W, F, W, F, W, F, W, F, W, F, W, F, W, F, W, F, W],
+    [W, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, W],
+    [W, F, W, F, W, F, W, F, W, F, W, F, W, F, W, F, W],
+    [W, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, W],
+    [W, F, W, F, W, F, W, F, W, F, W, F, W, F, W, F, W],
+    [W, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, W],
+    [W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W]
+  ],
+  [
+    [W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W],
+    [W, F, F, F, F, F, F, F, W, F, F, F, F, F, F, F, W],
+    [W, F, W, F, W, W, W, F, W, F, W, W, W, F, W, F, W],
+    [W, F, W, F, F, F, F, F, F, F, F, F, F, F, W, F, W],
+    [W, F, W, F, W, F, W, F, W, F, W, F, W, F, W, F, W],
+    [W, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, W],
+    [W, F, W, F, W, F, W, F, W, F, W, F, W, F, W, F, W],
+    [W, F, F, F, F, F, F, F, W, F, F, F, F, F, F, F, W],
+    [W, F, W, F, W, F, W, F, W, F, W, F, W, F, W, F, W],
+    [W, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, W],
+    [W, F, W, F, W, F, W, F, W, F, W, F, W, F, W, F, W],
+    [W, F, W, F, F, F, F, F, F, F, F, F, F, F, W, F, W],
+    [W, F, W, F, W, W, W, F, W, F, W, W, W, F, W, F, W],
+    [W, F, F, F, F, F, F, F, W, F, F, F, F, F, F, F, W],
+    [W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W]
+  ],
+  [
+    [W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W],
+    [W, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, W],
+    [W, F, W, F, W, F, W, F, W, F, W, F, W, F, W, F, W],
+    [W, F, F, F, W, F, F, F, W, F, F, F, W, F, F, F, W],
+    [W, F, W, F, W, F, W, F, W, F, W, F, W, F, W, F, W],
+    [W, F, F, F, F, F, F, F, W, F, F, F, F, F, F, F, W],
+    [W, F, W, W, W, W, W, F, W, F, W, W, W, W, W, F, W],
+    [W, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, W],
+    [W, F, W, W, W, W, W, F, W, F, W, W, W, W, W, F, W],
+    [W, F, F, F, F, F, F, F, W, F, F, F, F, F, F, F, W],
+    [W, F, W, F, W, F, W, F, W, F, W, F, W, F, W, F, W],
+    [W, F, F, F, W, F, F, F, W, F, F, F, W, F, F, F, W],
+    [W, F, W, F, W, F, W, F, W, F, W, F, W, F, W, F, W],
+    [W, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, W],
+    [W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W]
+  ],
+  [
+    [W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W],
+    [W, F, F, F, W, F, F, F, W, F, F, F, W, F, F, F, W],
+    [W, F, W, F, F, F, W, F, F, F, W, F, F, F, W, F, W],
+    [W, F, F, F, W, F, F, F, W, F, F, F, W, F, F, F, W],
+    [W, F, W, F, F, F, W, F, F, F, W, F, F, F, W, F, W],
+    [W, F, F, F, W, F, F, F, W, F, F, F, W, F, F, F, W],
+    [W, F, W, F, F, F, W, F, F, F, W, F, F, F, W, F, W],
+    [W, F, F, F, W, F, F, F, W, F, F, F, W, F, F, F, W],
+    [W, F, W, F, F, F, W, F, F, F, W, F, F, F, W, F, W],
+    [W, F, F, F, W, F, F, F, W, F, F, F, W, F, F, F, W],
+    [W, F, W, F, F, F, W, F, F, F, W, F, F, F, W, F, W],
+    [W, F, F, F, W, F, F, F, W, F, F, F, W, F, F, F, W],
+    [W, F, W, F, F, F, W, F, F, F, W, F, F, F, W, F, W],
+    [W, F, F, F, W, F, F, F, W, F, F, F, W, F, F, F, W],
+    [W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W]
+  ],
+  [
+    [W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W],
+    [W, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, W],
+    [W, F, W, F, W, F, W, F, W, F, W, F, W, F, W, F, W],
+    [W, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, W],
+    [W, F, W, F, W, F, W, W, W, W, W, F, W, F, W, F, W],
+    [W, F, F, F, F, F, W, F, F, F, W, F, F, F, F, F, W],
+    [W, F, W, W, W, W, W, F, F, F, W, W, W, W, W, F, W],
+    [W, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, W],
+    [W, F, W, W, W, W, W, F, F, F, W, W, W, W, W, F, W],
+    [W, F, F, F, F, F, W, F, F, F, W, F, F, F, F, F, W],
+    [W, F, W, F, W, F, W, W, W, W, W, F, W, F, W, F, W],
+    [W, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, W],
+    [W, F, W, F, W, F, W, F, W, F, W, F, W, F, W, F, W],
+    [W, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, W],
+    [W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W]
+  ]
 ]
-
-export const collisionMap = [
-  [CW, CW, CW, CW, CW, CW, CW, CW, CW, CW, CW, CW, CW, CW, CW, CW, CW],
-  [CW, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CW],
-  [CW, CE, CW, CE, CW, CE, CW, CE, CW, CE, CW, CE, CW, CE, CW, CE, CW],
-  [CW, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CW],
-  [CW, CE, CW, CE, CW, CE, CW, CE, CW, CE, CW, CE, CW, CE, CW, CE, CW],
-  [CW, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CW],
-  [CW, CE, CW, CE, CW, CE, CW, CE, CW, CE, CW, CE, CW, CE, CW, CE, CW],
-  [CW, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CW],
-  [CW, CE, CW, CE, CW, CE, CW, CE, CW, CE, CW, CE, CW, CE, CW, CE, CW],
-  [CW, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CW],
-  [CW, CE, CW, CE, CW, CE, CW, CE, CW, CE, CW, CE, CW, CE, CW, CE, CW],
-  [CW, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CW],
-  [CW, CE, CW, CE, CW, CE, CW, CE, CW, CE, CW, CE, CW, CE, CW, CE, CW],
-  [CW, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CE, CW],
-  [CW, CW, CW, CW, CW, CW, CW, CW, CW, CW, CW, CW, CW, CW, CW, CW, CW]
-]
-
