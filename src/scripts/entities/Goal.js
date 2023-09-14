4020a8c7d61d6263f9a1cdad7384bc4f7d3f7165
IvaanMiguel <mdominguez_20@alu.uabcs.mx>
2023-09-14 11:01:08 -0700

    Clase de entidad añadida.

---
+++ src/scripts/entities/Goal.js
@@ -0,0 +1,81 @@
+import {
+  CollisionTile,
+  FRAME_TIME,
+  Sprite,
+  TILE_SIZE,
+  Tile
+} from '../constants/game.js';
+
+import Entity from './Entity.js';
+import { getSprite } from './utils.js';
+
+const FRAME_DELAY = 16
+const FIRST_FRAME = 4
+const GoalAnimation = [0, 1, 2, 3]
+
+class Goal extends Entity {
+  image = Sprite.TILES
+  animation = GoalAnimation
+
+  constructor(levelMap, time) {
+    super({ x: 0, y: 0 })
+
+    this.levelMap = levelMap
+    this.cell = { row: 0, col: 0 }
+    this.isBlocked = true
+
+    this.animationTimer = time.previous + FRAME_DELAY * FRAME_TIME
+
+    this.createGoal()
+  }
+
+  createGoal() {
+    const index = Math.floor(Math.random() * this.levelMap.blocks.length)
+
+    this.cell = this.levelMap.blocks[index]
+    this.levelMap.tileMap[this.cell.row][this.cell.col] = Tile.GOAL
+    this.position = {
+      x: this.cell.col * TILE_SIZE,
+      y: this.cell.row * TILE_SIZE
+    }
+  }
+
+  resetGoal() {
+    this.isBlocked = true
+    this.animationFrame = 0
+    this.createGoal()
+  }
+
+  updateAnimation(time) {
+    if (time.previous < this.animationTimer) return
+
+    this.animationFrame += 1    
+    if (this.animationFrame >= this.animation.length) this.animationFrame = 0
+
+    this.animationTimer = time.previous + FRAME_DELAY * FRAME_TIME
+  }
+
+  update(time) {
+    this.isBlocked = this.levelMap.collisionMap[this.cell.row][this.cell.col] === CollisionTile.BARRIER.BLOCK
+
+    if (this.isBlocked) return
+
+    this.updateAnimation(time)
+  }
+
+  draw(ctx) {
+    if (this.isBlocked) return
+
+    const { originX, originY } = getSprite(this.image, FIRST_FRAME + this.animation[this.animationFrame])
+
+    ctx.drawImage(
+      this.image,
+      originX, originY,
+      TILE_SIZE, TILE_SIZE,
+      this.position.x, this.position.y,
+      TILE_SIZE, TILE_SIZE
+    )
+  }
+}
+
+export default Goal
