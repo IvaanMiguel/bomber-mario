792b9485ff69b0c468994bc7a7d00741101a67a6
IvaanMiguel <mdominguez_20@alu.uabcs.mx>
2023-09-14 10:59:16 -0700

    Entidad Goal añadida.

--- src/scripts/core/Scene.js
+++ src/scripts/core/Scene.js
@@ -3,6 +3,7 @@ import { SCREEN_HEIGHT, SCREEN_WIDTH } from '../constants/game.js'
 import Player from '../entities/Player.js'
 import LevelMap from '../entities/LevelMap.js'
 import BombsSystem from './BombsSystem.js'
+import Goal from '../entities/Goal.js'
 
 class Scene {
   constructor(canvas, time) {
@@ -11,10 +12,14 @@ class Scene {
     this.ctx.imageSmoothingEnabled = false;
 
     this.levelMap = new LevelMap()
+    this.goal = new Goal(this.levelMap, time)
+
     this.bombsSystem = new BombsSystem(this.levelMap)
+
     this.player = new Player(
       { row: 1, col: 1 },
       this.levelMap,
+      this.goal,
       this.bombsSystem.add,
       this.onGoalReached,
       time
@@ -24,6 +29,7 @@ class Scene {
   onGoalReached = (time) => {
     this.player.restartPlayer(time)
     this.levelMap.regenMap()
+    this.goal.resetGoal()
     this.bombsSystem.resetBombs()
   }
 
@@ -31,11 +37,13 @@ class Scene {
     this.ctx.clearRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT)
 
     this.levelMap.draw(this.ctx)
+    this.goal.draw(this.ctx)
     this.bombsSystem.draw(this.ctx)
     this.player.draw(this.ctx)
   }
 
   update(time) {
+    this.goal.update(time)
     this.bombsSystem.update(time)
     this.player.update(time)
   }
