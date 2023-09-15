63877af807e98c6fd36606bc2ac499f097503b3f
IvaanMiguel <mdominguez_20@alu.uabcs.mx>
2023-09-14 23:33:06 -0700

    Pausa implementada.

--- src/scripts/core/Game.js
+++ src/scripts/core/Game.js
@@ -2,6 +2,7 @@ import { addKeyEventsListeners } from './inputHandler.js'
 import { CANVAS_ID, SCREEN_HEIGHT, SCREEN_WIDTH } from '../constants/game.js'
 
 import Scene from './Scene.js'
+import Pause from '../screens/Pause.js'
 
 class Game {
   canvas = document.getElementById(CANVAS_ID)
@@ -12,13 +13,21 @@ class Game {
     this.canvas.height = SCREEN_HEIGHT
 
     this.scene = new Scene(canvas, this.frameTime)
+    this.pauseScreen = new Pause()
   }
 
   update = timestamp => {
     window.requestAnimationFrame(this.update)
 
-    this.frameTime.secondsPassed = (timestamp - this.frameTime.previous) / 1000
-		this.frameTime.previous = timestamp
+    this.pauseScreen.update(this.frameTime)
+    this.pauseScreen.draw(this.canvas.getContext('2d'))
+
+    if (this.pauseScreen.isActive) return
+
+    this.pauseScreen.updateTimeOnScreen(timestamp)
+
+    this.frameTime.secondsPassed = ((timestamp - this.pauseScreen.timeOnScreen) - this.frameTime.previous) / 1000
+		this.frameTime.previous = timestamp - this.pauseScreen.timeOnScreen
 
     this.scene.update(this.frameTime)
     this.scene.draw()
