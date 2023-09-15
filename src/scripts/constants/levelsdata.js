7cd2bc0cb099a6dcb75f76cea1838dcc02137835
IvaanMiguel <mdominguez_20@alu.uabcs.mx>
2023-09-15 12:57:37 -0700

    Mapa añadido

--- src/scripts/constants/levelsdata.js
+++ src/scripts/constants/levelsdata.js
@@ -88,5 +88,42 @@ export default [
     [W, F, W, F, W, F, W, F, W, F, W, F, W, F, W, F, W],
     [W, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, W],
     [W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W]
+  ],
+  [
+    [W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W],
+    [W, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, W],
+    [W, F, F, F, F, F, F, W, F, W, F, F, F, F, F, F, W],
+    [W, F, F, F, F, W, W, F, F, F, W, W, F, F, F, F, W],
+    [W, F, W, F, W, F, F, F, F, F, F, F, W, F, W, F, W],
+    [W, F, F, W, F, F, F, W, W, F, F, F, F, W, F, F, W],
+    [W, F, F, W, F, F, F, F, F, W, F, F, F, W, F, F, W],
+    [W, F, W, F, F, F, F, W, W, F, F, F, F, F, W, F, W],
+    [W, F, F, W, F, F, F, F, F, W, F, F, F, W, F, F, W],
+    [W, F, W, F, F, F, F, W, W, F, F, F, F, F, W, F, W],
+    [W, F, F, W, F, F, F, F, F, F, F, F, F, W, F, F, W],
+    [W, F, F, F, W, F, F, W, W, W, F, F, W, F, F, F, W],
+    [W, F, F, W, F, W, W, F, W, F, W, W, F, W, F, F, W],
+    [W, F, F, F, F, F, F, F, W, F, F, F, F, F, F, F, W],
+    [W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W]
   ]
 ]
+
+/* Para crear mapas más fácilmente.
+[
+  [W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W],
+  [W, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, W],
+  [W, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, W],
+  [W, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, W],
+  [W, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, W],
+  [W, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, W],
+  [W, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, W],
+  [W, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, W],
+  [W, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, W],
+  [W, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, W],
+  [W, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, W],
+  [W, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, W],
+  [W, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, W],
+  [W, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, W],
+  [W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W]
+]
+*/
--- src/scripts/entities/Entity.js
+++ src/scripts/entities/Entity.js
@@ -1,5 +1,4 @@
 class Entity {
-  position = { x: 0, y: 0 }
   velocity = { x: 0, y: 0 }
 
   animationFrame = 0
