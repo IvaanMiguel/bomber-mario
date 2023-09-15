817780b0ce16e5765f7f96b937617a2f83bf19bc
IvaanMiguel <mdominguez_20@alu.uabcs.mx>
2023-09-15 08:44:51 -0700

    Contenido de pantalla de pausa añadido.

--- src/scripts/screens/Pause.js
+++ src/scripts/screens/Pause.js
@@ -1,15 +1,25 @@
 import {
+  ControlsActionsCoords,
+  ControlsActionsDimensions,
+  ControlsMovementCoords,
+  ControlsMovementDimensions,
+  ControlsTitleCoords,
+  ControlsTitleDimensions,
   PAUSE_BG_COLOR,
   PAUSE_SCREEN_HEIGHT,
   PAUSE_SCREEN_WIDTH,
+  PauseTItleDimensions,
+  PauseTitleCoords,
   SCREEN_HEIGHT,
-  SCREEN_WIDTH
+  SCREEN_WIDTH,
+  Sprite
 } from '../constants/game.js';
 import { Control } from '../constants/playermovement.js';
 import { isKeyPressed } from '../core/inputHandler.js';
 import Entity from '../entities/Entity.js';
 
 class Pause extends Entity {
+  image = Sprite.PAUSE_SCREEN
   timeOnScreen = 0
   initialTime = 0
 
@@ -46,9 +56,24 @@ class Pause extends Entity {
     this.wasPaused = true
   }
 
+  drawScreenSection(dimensions, coords) {
+    this.ctx.drawImage(
+      this.image,
+      dimensions[0], dimensions[1],
+      dimensions[2], dimensions[3],
+      coords[0], coords[1],
+      dimensions[2], dimensions[3]
+    )
+  }
+
   createScreen() {
     this.ctx.fillStyle = PAUSE_BG_COLOR
     this.ctx.fillRect(0, 0, this.width, this.height)
+
+    this.drawScreenSection(PauseTItleDimensions, PauseTitleCoords)
+    this.drawScreenSection(ControlsTitleDimensions, ControlsTitleCoords)
+    this.drawScreenSection(ControlsMovementDimensions, ControlsMovementCoords)
+    this.drawScreenSection(ControlsActionsDimensions, ControlsActionsCoords)
   }
 
   update(time) {
